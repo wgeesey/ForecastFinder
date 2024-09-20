a942687d4824361c00b751dfe30e75bc78c7613e
will <williamgeesey@gmail.com>
2024-09-20 13:42:10 -0400

    Program runs on local system.

---
+++ forecastfinder/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from .forecast_finder import forecast_finder
+
+# Create your views here.
+
+
+def index(request):
+    forecast_result = ""
+    if request.method == 'POST':
+        location = request.POST.get('location')
+        num_days = request.POST.get('num_days')
+        if location and num_days:
+            print(f"Location: {location}, Number of Days: {num_days}")
+            forecast_result = forecast_finder(location, int(num_days))
+    return render(request, 'index.html', {'forecast': forecast_result})
